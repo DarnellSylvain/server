45e34a6e2284dbc17e19ad52245f6331aefbaffa
Darnell <darnellsylvain@googlemail.com>
2024-08-03 10:46:09 +0100

    renamed import

--- src/users/users.routes.ts
+++ src/users/users.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { createUserHandler } from "./users.controller";
-import validate from "src/middleware/validatedResource";
+import validate from "src/middleware/validated-resource";
 import { createUserSchema } from "./users.schemas";
 
 const router = Router();
